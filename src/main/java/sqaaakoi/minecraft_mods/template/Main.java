5017c3b1a6b5841ad1c4a5f34c6dcd68c9222fc8
Sqaaakoi <37475903+Sqaaakoi@users.noreply.github.com>
2021-10-16 14:06:17 +1300

    Trash from last night.

---
+++ src/main/java/sqaaakoi/minecraft_mods/template/Main.java
@@ -0,0 +1,15 @@
+package sqaaakoi.minecraft_mods.template;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main implements ModInitializer {
+  public static final String ID = "template";
+  public static final Logger LOG = LogManager.getLogger(ID);
+
+  @Override
+  public void onInitialize() {
+    LOG.debug("Loaded mod {}", ID);
+  }
+}
---
+++ src/main/java/sqaaakoi/minecraft_mods/template/mixins/ExampleMixin.java
@@ -0,0 +1,22 @@
+package sqaaakoi.minecraft_mods.template.mixins;
+
+import sqaaakoi.minecraft_mods.template.Main;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.objectweb.asm.Opcodes;
+import javax.annotation.Nullable;
+import net.minecraft.client.gui.screen.TitleScreen;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+  @Inject(at = @At("HEAD"), method = "init()V")
+  private void init(CallbackInfo info) {
+    // System.out.println("This line is printed by an example mod mixin!");
+  }
+}
