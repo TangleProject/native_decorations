f30ca07ce4e1dbafe3f3ce8d9a0cf9a110259f84
Sqaaakoi <37475903+Sqaaakoi@users.noreply.github.com>
2022-01-15 15:07:26 +1300

    fix some issues with generation

--- src/main/java/sqaaakoi/minecraft_mods/native_decorations/world/gen/features/Features.java
+++ src/main/java/sqaaakoi/minecraft_mods/native_decorations/world/gen/features/Features.java
@@ -2,14 +2,17 @@ package sqaaakoi.minecraft_mods.native_decorations.world.gen.features;
 
 import sqaaakoi.minecraft_mods.native_decorations.Main;
 import sqaaakoi.minecraft_mods.native_decorations.blocks.Blocks;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.util.math.intprovider.ConstantIntProvider;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.Heightmap;
-import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.PlacedFeature;
 import net.minecraft.world.gen.decorator.HeightmapPlacementModifier;
 import net.minecraft.world.gen.decorator.PlacementModifierType;
@@ -24,7 +27,7 @@ public class Features {
   public static final RegistryKey<ConfiguredFeature<?, ?>> key_configured_cobblestone_rock = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY, new Identifier(Main.ID, "cobblestone_rock"));
 
   public static final PlacedFeature placed_cobblestone_rock = configured_cobblestone_rock.withPlacement(HeightmapPlacementModifier.of(Heightmap.Type.OCEAN_FLOOR_WG), RarityFilterPlacementModifier.of(6));
-  public static final RegistryKey<ConfiguredFeature<?, ?>> key_placed_cobblestone_rock = RegistryKey.of(Registry.PLACED_FEATURE_KEY, new Identifier(Main.ID, "cobblestone_rock"));
+  public static final RegistryKey<PlacedFeature> key_placed_cobblestone_rock = RegistryKey.of(Registry.PLACED_FEATURE_KEY, new Identifier(Main.ID, "cobblestone_rock"));
 
   public static void register() {
     Registry.register(Registry.FEATURE, new Identifier(Main.ID, "rock"), feature_rock);
