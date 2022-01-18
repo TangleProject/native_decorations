a0736ed279fd7fe46a93ddaac2f77138d6b56c23
Sqaaakoi <37475903+Sqaaakoi@users.noreply.github.com>
2022-01-18 19:23:29 +1300

    i think i am being trolled by my text editor

--- src/main/java/sqaaakoi/minecraft_mods/native_decorations/world/biome/Biomes.java
+++ src/main/java/sqaaakoi/minecraft_mods/native_decorations/world/biome/Biomes.java
@@ -63,7 +63,7 @@ public class Biomes {
     DefaultBiomeFeatures.addForestGrass(gens);
     //  Custom trees (oak only)
     gens.feature(GenerationStep.Feature.VEGETAL_DECORATION, Features.placed_trees_big_oak);
-    return new Biome.Builder().precipitation(Biome.Precipitation.RAIN).category(Biome.Category.FOREST).temperature(0.7f).downfall(0.8f).effects(new BiomeEffects.Builder().grassColor(0x56d24f).foliageColor(0x14b011).waterColor(0x38a5db).waterFogColor(0x  032436).fogColor(0xc0d8ff).skyColor(OverworldBiomeCreator.getSkyColor(0.7f)).moodSound(BiomeMoodSound.CAVE).music(null).build()).spawnSettings(sps.build()).generationSettings(gens.build()).build();
+    return new Biome.Builder().precipitation(Biome.Precipitation.RAIN).category(Biome.Category.FOREST).temperature(0.7f).downfall(0.8f).effects(new BiomeEffects.Builder().grassColor(0x56d24f).foliageColor(0x14b011).waterColor(0x38a5db).waterFogColor(0x032436).fogColor(0xc0d8ff).skyColor(OverworldBiomeCreator.getSkyColor(0.7f)).moodSound(BiomeMoodSound.CAVE).music(null).build()).spawnSettings(sps.build()).generationSettings(gens.build()).build();
   }
 
   public static void register() {
